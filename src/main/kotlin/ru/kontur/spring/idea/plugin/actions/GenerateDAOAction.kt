4772c5a9c3a32fae951c939c26053bdf82b54f4d
kostya05983 <kostya05983@mail.ru>
2019-09-29 01:27:05 +0700

    Init with action

---
+++ src/main/kotlin/ru/kontur/spring/idea/plugin/actions/GenerateDAOAction.kt
@@ -0,0 +1,12 @@
+package ru.kontur.spring.idea.plugin.actions
+
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GenerateDAOAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
